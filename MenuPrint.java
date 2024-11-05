dee3376e366ee745994f783b5e9712da38397706
Manhcodee <phammanh2952@gmail.com>
2024-11-05 14:36:13 +0700

    first

---
+++ MenuPrint.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class MenuPrint {
+    public static void main(String[] args) {
+        int choice = -1;
+        Scanner scanner = new Scanner(System.in);
+        while (choice != 0) {
+            System.out.println("Menu");
+            System.out.println("1. Draw the square");
+            System.out.println("2. Print the square triangle");
+            System.out.println("3. Print isosceles triangle");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Draw the square");
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 7; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 2:
+                    System.out.println("2. Print the square triangle");
+                    for (int i = 0; i < 5; i++) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("* ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    System.out.println("3. Print isosceles triangle");
+                    for (int i = 5; i > 0; i--) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("No choice");
+            }
+        }
+        scanner.close();
+    }
+}
